6750dd89fa4f9e85d5bd1bf8f37c6ba5558943ba
Pham Nguyen Phu Thanh <thanhphamnguyenphu2k5@gmail.com>
2026-03-16 10:27:19 +0000

    Improve(Project): beautify result, optimize & enhance threads workflow

--- crawler_app/app.py
+++ crawler_app/app.py
@@ -7,16 +7,48 @@ from io import StringIO
 from threading import Thread
 from flask import Flask, request, render_template, make_response
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 def crawl(url, parser, results):
     try:
         response = requests.get(url)
         dfs = pd.read_html(StringIO(response.text), flavor=f'{parser}')
         for df in dfs:
-            df["Ngày xuất"] = datetime.now()
+            # Xóa https ở cuối table (nếu có)
             last_row = str(df.iloc[-1,0])
             if ("http" in last_row or "https" in last_row):
                 df = df.drop(df.index[-1])
+
+            #Xóa cột Unnamed: 4 (nếu có)
+            if ('Unnamed: 4' in df.columns):
+                df = df.drop(columns=['Unnamed: 4'])
+
+            # kiểm cột khu vực của PNJ (nếu có)
+            df = df[df['Khu vực'] != 'Giá vàng nữ trang']
+
+            # format lại tất cả cột thời gian cập nhật (nếu có)
+            time_column = None
+            for i in range(df.shape[1]):
+                if ('Thời gian' in df.columns[i]):
+                    time_column = df.columns[i]
+
+            print(time_column)
+            if (time_column != None):
+                try:
+                    for i in range(df.shape[0]):
+                        og_time = datetime.strptime(df.loc[i,time_column], '%H:%M:%S %d/%m/%Y').date()
+                        format_time = og_time.strftime('%d-%m-%Y')
+                        df.loc[i,time_column] = format_time
+                except Exception as e:
+                    print(e)
+
+            # Thêm cột ngày xuất file
+            df["Ngày xuất"] = datetime.now().strftime('%d-%m-%Y')
+            print(df.columns)
+
+            # Gộp cột tgian & tgian cập nhật
+            df = df.rename(columns={'Thời gian cập nhật': 'Thời gian'})
+
             results.append(df)
 
         return True
@@ -48,19 +80,33 @@ def index():
 
         results = []
 
-        threads = []
-        for i in range(days_amount):
-            t = Thread(target=crawl, args=(url, parser, results))
-            threads.append(t)
-            t.start()
-            url = auto_date(url)
-            if(url == None): break
+        # threads = []
+        # for i in range(days_amount):
+        #     t = Thread(target=crawl, args=(url, parser, results))
+        #     threads.append(t)
+        #     t.start()
+        #     url = auto_date(url)
+        #     # if(url == None): break
 
-        for t in threads:
-            t.join()
+        # for t in threads:
+        #     t.join()
+
+        # Improve Threads
+        urls = []
+        for i in range(days_amount):
+            urls.append(url)
+            url = auto_date(url)
+
+        with ThreadPoolExecutor (max_workers=20) as executor:
+            for u in urls:
+                executor.submit(crawl, u, parser, results)
 
         if (results):
             final_df = pd.concat(results, ignore_index=True)
+            if ('Thời gian' in final_df.columns):
+                final_df['Thời gian'] = pd.to_datetime(final_df['Thời gian'], format='%d-%m-%Y')
+                final_df = final_df.sort_values(by='Thời gian', ascending=True)
+                final_df['Thời gian'] = final_df['Thời gian'].dt.strftime('%d-%m-%Y')
             csv_string = final_df.to_csv(index=False)
 
             response = make_response(csv_string)
